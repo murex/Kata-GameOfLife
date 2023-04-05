f601fa37d28414362d4d4aeb6daa80a1b9905c8b
Damien Menanteau <damien.menanteau@gmail.com>
2023-04-05 10:35:35 +0200

    Change python test framework from unittest to pytest

--- python/tests/game_of_life_test.py
+++ python/tests/game_of_life_test.py
@@ -1,59 +1,54 @@
-import unittest
-
-from parameterized import parameterized
+import pytest
+from parametrization import Parametrization
 from game_of_life.game_of_life import generation, count_live_neighbors
 
 
-class GameOfLifeTest(unittest.TestCase):
+class TestGameOfLife:
 
-    @unittest.skip("TODO: bottom-up TDD")  # Comment or remove this line to enable this test case
+    @pytest.mark.skip(reason="TODO: bottom-up TDD")  # Comment or remove this line to enable this test case
     def test_count_alive_neighboring_cells_first_test(self):
         pass
 
-    @unittest.skip("TODO: top-down TDD")  # Comment or remove this line to enable this test case
+    @pytest.mark.skip(reason="TODO: top-down TDD")  # Comment or remove this line to enable this test case
     def test_game_of_life_first_test(self):
         pass
 
-    @parameterized.expand([
-        ("blinker",
-         [
-             "_____",
-             "__O__",
-             "__O__",
-             "__O__",
-             "_____",
-         ],
-         [
-             "_____",
-             "_____",
-             "_OOO_",
-             "_____",
-             "_____",
-         ]),
-        ("toad",
-         [
-             "______",
-             "______",
-             "__OOO_",
-             "_OOO__",
-             "______",
-             "______",
-         ],
-         [
-             "______",
-             "___O__",
-             "_O__O_",
-             "_O__O_",
-             "__O___",
-             "______",
-         ]),
-
-    ])
-    @unittest.skip("test currently disabled")  # Comment or remove this line to enable this test case
-    def test_oscillator_acceptance_tests(self, _name, world1, world2):
-        self.assertEqual(world2, generation(world1))
-        self.assertEqual(world1, generation(world2))
-
-
-if __name__ == "__main__":
-    unittest.main()
+    @pytest.mark.skip(reason="test currently disabled")  # Comment or remove this line to enable this test case
+    @Parametrization.autodetect_parameters()
+    @Parametrization.case(
+        name="blinker",
+        world1=[
+            "_____",
+            "__O__",
+            "__O__",
+            "__O__",
+            "_____",
+        ],
+        world2=[
+            "_____",
+            "_____",
+            "_OOO_",
+            "_____",
+            "_____",
+        ])
+    @Parametrization.case(
+        name="toad",
+        world1=[
+            "______",
+            "______",
+            "__OOO_",
+            "_OOO__",
+            "______",
+            "______",
+        ],
+        world2=[
+            "______",
+            "___O__",
+            "_O__O_",
+            "_O__O_",
+            "__O___",
+            "______",
+        ])
+    def test_oscillator_acceptance_tests(self, world1, world2):
+        assert (world2 == generation(world1))
+        assert (world1 == generation(world2))
