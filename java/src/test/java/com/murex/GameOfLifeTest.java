5d6d14ab4d753fa7c15f4f1735e8556f74c874ae
Damien Menanteau <damien.menanteau@gmail.com>
2023-04-14 18:33:33 +0200

    Upgrade java bootstraps to junit 5

--- java/src/test/java/com/murex/GameOfLifeTest.java
+++ java/src/test/java/com/murex/GameOfLifeTest.java
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 2021 Murex
+Copyright (c) 2022 Murex
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -22,66 +22,64 @@ SOFTWARE.
 
 package com.murex;
 
-import org.junit.Ignore;
-import org.junit.Test;
-
-import static org.junit.Assert.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
 
 public class GameOfLifeTest {
-
-    @Ignore
+    
+    @Disabled
     @Test
     public void blinker_oscillator_acceptance_test() {
         String[] world1 = {
-                "_____",
-                "__O__",
-                "__O__",
-                "__O__",
-                "_____",
+            "_____",
+            "__O__",
+            "__O__",
+            "__O__",
+            "_____",
         };
         String[] world2 = {
-                "_____",
-                "_____",
-                "_OOO_",
-                "_____",
-                "_____",
+            "_____",
+            "_____",
+            "_OOO_",
+            "_____",
+            "_____",
         };
         assertArrayEquals(world2, GameOfLife.generation(world1));
         assertArrayEquals(world1, GameOfLife.generation(world2));
     }
-
-    @Ignore
+    
+    @Disabled
     @Test
     public void toad_oscillator_acceptance_test() {
         String[] world1 = {
-                "______",
-                "______",
-                "__OOO_",
-                "_OOO__",
-                "______",
-                "______",
+            "______",
+            "______",
+            "__OOO_",
+            "_OOO__",
+            "______",
+            "______",
         };
         String[] world2 = {
-                "______",
-                "___O__",
-                "_O__O_",
-                "_O__O_",
-                "__O___",
-                "______",
+            "______",
+            "___O__",
+            "_O__O_",
+            "_O__O_",
+            "__O___",
+            "______",
         };
         assertArrayEquals(world2, GameOfLife.generation(world1));
         assertArrayEquals(world1, GameOfLife.generation(world2));
     }
-
-
+    
     /* Bottom-up */
-
+    
     @Test
     public void count_alive_neighboring_cells_first_test() {
     }
-
+    
     /* Then top-down */
-
+    
     @Test
     public void game_of_life_first_test() {
     }
